25334737b7d885eb803d7e1bd0c22dcbc2b2a7a9
Karol <mouracinthia1@gmail.com>
2021-11-17 22:12:19 -0300

    Exercicio de POO

---
+++ Nome.java
@@ -0,0 +1,10 @@
+package POO;
+
+public class Nome {
+	public static void main (String args[])
+	{
+		Pessoa pessoa1 = new Pessoa("Ana","Henrique","Cristiano");
+		System.out.println(pessoa1.getNomeCompleto());
+	}
+
+}
